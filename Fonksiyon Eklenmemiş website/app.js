4148a8ed4f970aa301b076cbf263d8aabab42775
D3NIZ1921 <wris8789@outlook.com>
2023-10-20 16:17:27 +0300

    OnePage Website
    html css

---
+++ Fonksiyon Eklenmemiş website/app.js
@@ -0,0 +1,15 @@
+alert("Hoşgeldiniz, keyifli alışverişler dileriz!");
+
+function tepkiVer(){
+
+alert("İşlem gerçekleşti");
+
+};
+function tekrarBekleriz(){
+
+
+alert("Tekrar bekleriz!")
+
+
+
+};
